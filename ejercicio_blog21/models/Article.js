0020d87798b2493664c824ccf369ff1bc153cdcc
nodriozola <nicolasodriozola1@gmail.com>
2022-02-10 15:07:35 -0300

    inicio de proyecto

---
+++ ejercicio_blog21/models/Article.js
@@ -0,0 +1,28 @@
+module.exports = (sequelize, Model, DataTypes) => {
+  class Article extends Model {}
+
+  Article.init(
+    {
+      id: {
+        type: DataTypes.BIGINT.UNSIGNED,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      title: {
+        type: DataTypes.STRING,
+      },
+      content: {
+        type: DataTypes.TEXT,
+      },
+      image: {
+        type: DataTypes.STRING,
+      },
+    },
+    {
+      sequelize,
+      modelName: "article",
+    },
+  );
+
+  return Article;
+};
