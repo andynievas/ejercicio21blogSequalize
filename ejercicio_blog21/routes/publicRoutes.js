e2e24329ec135dd5514135f5398c640a327356e8
Andy Nievas <nievasandy73@gmail.com>
2022-02-10 15:36:33 -0300

    agrego la ruta para el articulo

--- ejercicio_blog21/routes/publicRoutes.js
+++ ejercicio_blog21/routes/publicRoutes.js
@@ -8,4 +8,9 @@ publicRouter.get("/", (req, res)=>{
     res.render("index");
 });
 
+publicRouter.get("/articulo/:id", (req, res)=>{
+    console.log( "Se recibio el id", req.params.id );
+    res.render("index");
+});
+
 module.exports = publicRouter;
