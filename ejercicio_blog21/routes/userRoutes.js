71294eac30baef95d814f99dd2c662b81f80a59b
maxiCaballo <caballomaxi@gmail.com>
2022-02-15 15:20:08 -0300

    Arreglo maxi

--- ejercicio_blog21/routes/userRoutes.js
+++ ejercicio_blog21/routes/userRoutes.js
@@ -6,4 +6,10 @@ const userController = require("../controllers/userController");
 
 //RUTAS :D
 
+userRouter.get("/registro", "controlador");
+userRouter.post("/registro", "controlador");
+userRouter.get("/login", "controlador");
+userRouter.post("/login", "controlador");
+userRouter.get("/logout", "controlador");
+
 module.exports = userRouter;
--- ejercicio_blog21/server.js
+++ ejercicio_blog21/server.js
@@ -5,6 +5,9 @@ const routes = require("./routes");
 const dbInitialSetup = require("./dbInitialSetup");
 const APP_PORT = process.env.APP_PORT || 3000;
 const app = express();
+const session = require("express-session");
+const passport = require("passport");
+const localStrategy = require("passport-local");
 
 app.use(express.json());
 app.use(express.static("public"));
