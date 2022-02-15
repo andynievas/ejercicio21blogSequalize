b9b6b0efa094801a5d8ad57a7782b3b83ac3d541
Adrian Del Valle <adrianchelo7@gmail.com>
2022-02-15 16:34:54 -0300

    "Arreglos"

--- ejercicio_blog21/server.js
+++ ejercicio_blog21/server.js
@@ -7,12 +7,62 @@ const APP_PORT = process.env.APP_PORT || 3000;
 const app = express();
 const session = require("express-session");
 const passport = require("passport");
-const localStrategy = require("passport-local");
+const LocalStrategy = require("passport-local");
 
 app.use(express.json());
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+
+app.use(
+  session({
+    secret: "1234",
+    resave: false,
+    saveUninitialized: false,
+  }),
+);
+
+app.use(passport.session());
+
+passport.use(
+  new LocalStrategy(function (username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) {
+        return done(err);
+      }
+
+      if (!user) {
+        return done(null, false, { message: "Incorrect username." });
+      }
+
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: "Incorrect password." });
+      }
+
+      return done(null, user);
+    });
+  }),
+);
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+  User.findByPk(id)
+    .then((user) => {
+      done(null, user); // Usuario queda disponible en req.user.
+    })
+    .catch((error) => {
+      done(error, user);
+    });
+});
+
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/admin",
+    failureRedirect: "/login",
+  }),
+);
 
 routes(app);
 
